23df43b4a124913229def6a7ae0b45016ae7ba50
Oleg Gaidarenko <markelog@gmail.com>
2017-02-17 10:23:03 +0100

    Preparation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
+// External modules
 const Koa = require('koa');
 
+// Koa dependencies
+const bodyParser = require('koa-bodyparser');
+
+// Internal dependecies
+const config = require('./configs');
+
+// Routers
+
+// Middlewares
+const errors = require('./middlewares/errors');
+const logger = require('./middlewares/logger');
+
+// Initialize App
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello world';
+// Define the app
+app.use(logger());
+
+app.use(errors());
+app.use(bodyParser());
+
+app.listen(config.http.port, () => {
+  console.log('Started');
 });
 
-// app.listen(3000);
-
 module.exports = app;
-
-
--- test/integration/base/index.js
+++
@@ -1,5 +0,0 @@
-describe('GET /', function() {
-  it('respond with "hello world"', () => {
-    return request(app).get('/').expect(200);
-  });
-});
